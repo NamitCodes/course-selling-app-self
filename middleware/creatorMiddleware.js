6589e20043199063b4eb801f02c4cc2eebf12f02
Namit <namitguptadelhi@gmail.com>
2024-09-26 21:50:28 +0530

    fixed middleware

--- middleware/creatorMiddleware.js
+++ middleware/creatorMiddleware.js
@@ -5,14 +5,19 @@ function cookieJWTAuth(req,res,next){
     const token = req.cookies.token;
     try{
         const creator = jwt.verify(token, JSON_SECRET_KEY);
-        // console.log(creator);
-        req.body.id = creator.id;
-        next()
+        if (creator.id){
+            req.body.id = creator.id;
+            next()
+        } else {
+            res.status(403).json({
+                message: "You are not authenticated."
+            })
+        }
     }
     catch (e) {
         res.clearCookie("token")
         res.json({
-            message: "Error occurred"
+            message: "Error while authenticating"
         })
     }
 }

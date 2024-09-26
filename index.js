e32e7ee041b2b30a1c6fafe0c0324e3555d284dc
Namit <namitguptadelhi@gmail.com>
2024-09-26 22:00:57 +0530

    added and used express-rate-limit

--- index.js
+++ index.js
@@ -7,9 +7,18 @@ const { courseRouter } = require("./routes/course");
 const { default: mongoose } = require('mongoose');
 const cookieParser = require("cookie-parser");
 
+const rateLimit = require("express-rate-limit")
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
+app.use(limiter)            // rate limit
 
 app.use("/user", userRouter)
 app.use("/creator", creatorRouter)
